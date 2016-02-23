e17f4ce31f99dd7447748ae94923e1080db06776
liyi <liyi.9ban@gmail.com>
2016-02-23 18:09:37 +0800

    Initial commit

---
+++ src/main/java/com/nban/dbmodel/DbmodelApplication.java
@@ -0,0 +1,12 @@
+package com.nban.dbmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbmodelApplication.class, args);
+	}
+}
